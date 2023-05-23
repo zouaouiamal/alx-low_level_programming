b1ba4c0b6a547406811a0fdea54fd5874946ea88
zouaouiamal <amalabidi816@gmail.com>
2023-05-23 23:49:46 +0100

    ADD

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char str[] = "_putchar\n";
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowerCase
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
