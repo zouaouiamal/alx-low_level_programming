ecde007c07508834ea26eec1922db35ef79ec1dd
zouaouiamal <amalabidi816@gmail.com>
2023-05-25 20:22:28 +0100

    Add

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplate two numbers
+ * @a: int type number
+ * @b: int type number
+ * Return: the result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line of length n
+ * @n: the length of the line to be printed
+ */
+void print_line(int n)
+{
+while (n-- > 0)
+_putchar('_');
+_putchar('\n');
+}
