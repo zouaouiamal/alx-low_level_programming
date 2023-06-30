d16f89c1860f092602b368eca1999305ab8e2a75
zouaouiamal <amalabidi816@gmail.com>
2023-06-30 19:17:42 +0100

    Add

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,22 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * array_iterator - Executes a function given as a
- *                  parameter on each element of an array.
- * @array: The array.
- * @size: The size of array.
- * @action: A pointer to the function to be executed.
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
-for (i = 0; i < size; i++)
-{
-action(array[i]);
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
-}
+
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,43 @@
-#include <stdio.h>
+#include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-
 /**
- * main - Entry point. Calculates the result of an arithmetic operation
- *        between two integers.
- *
- * @argc: The number of arguments passed to the program.
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0 (Success).
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-int num1, num2, result;
-char *operator;
-int (*op_func)(int, int);
+	int num1, num2;
+	char *op;
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
 }
 
-num1 = atoi(argv[1]);
-operator = argv[2];
-num2 = atoi(argv[3]);
-
-
-op_func = get_op_func(operator);
-
-if (op_func == NULL)
-{
-printf("Error\n");
-exit(99);
-}
-
-result = op_func(num1, num2);
-printf("%d\n", result);
-return (0);
-}
