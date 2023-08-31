75a8b840fde0ba294ba216db116b44d9425b96be
zouaouiamal <amalabidi816@gmail.com>
2023-08-31 20:31:02 +0100

    add

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
+	int idx = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (; *b != '\0'; b++)
+	while (b[idx] != '\0')
 	{
-		if (*b != '0' && *b != '1')
+		if (b[idx] != '0' && b[idx] != '1')
 			return (0);
 
-		result = result * 2 + (*b - '0');
+		result = result * 2 + (b[idx] - '0');
+		idx++;
 	}
 
 	return (result);
